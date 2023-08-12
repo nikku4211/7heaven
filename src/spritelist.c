102a6e1ef475ee6c1e39dc940adff5f8cc25c61f
Nikku4211 <73254369+nikku4211@users.noreply.github.com>
2023-08-11 21:51:35 -0400

    Add files via upload

--- src/spritelist.c
+++ src/spritelist.c
@@ -5,12 +5,12 @@
 
 #pragma bank 255
 
-
+BANKREF(player_tiles)
 
 //player sprite tiles
 INCBIN(player_tiles, "res/7heavenplayerspritesheettspr.2bpp")
 
-
+BANKREF(zombie_tiles)
 
 //zombie sprite tiles
 INCBIN(zombie_tiles, "res/zombieenemyspritesheettspr.2bpp")
